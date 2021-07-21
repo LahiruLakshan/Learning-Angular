98adcbcb658b64c8c066cfc860a238ca4f6f8ae4
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-07-21 23:30:48 +0530

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learning-angular';
+  title = 'Learning Angular';
+  name = "lahiru";
+  lastName = "lakshan"
+  getName(){
+    return this.lastName;
+  }
+  obj={
+    name:"lahiru",
+    age:20
+  }
+  arr=["lahiru","lakshan",20]
 }
